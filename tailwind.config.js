80c9e052c40e1ad45317dfd19d3c6e9ea67b66c6
BRavan1407 <upadhyaypruthviraj131@gmail.com>
2024-05-25 11:56:17 +0530

    added toogle s/w

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
